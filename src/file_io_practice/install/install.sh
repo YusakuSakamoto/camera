040d9ba8e0fb9bc5a76085ee63adeae4774395e1
YusakuSakamoto <yskibouyskibou@gmail.com>
2015-09-28 00:53:32 +0900

    rm install

--- src/file_io_practice/install/install.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-ar r /usr/local/lib/sakamoto/libsakamoto.a Like_terminal.o
-sudo cp *.h /usr/local/include/sakamoto/
-sudo rm /usr/lib/pkgconfig/sakamoto.pc
-sudo cp *.pc /usr/lib/pkgconfig/
-sudo cp *.txt /usr/local/bin/sakamoto/
-echo "installation complete"
