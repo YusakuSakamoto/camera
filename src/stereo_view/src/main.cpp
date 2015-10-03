599b412c232c7203fa7b9c4dd8905494f06191f3
YusakuSakamoto <yskibouyskibou@gmail.com>
2015-10-03 16:23:12 +0900

    thread arrange

--- src/stereo_view/src/main.cpp
+++ src/stereo_view/src/main.cpp
@@ -3,6 +3,79 @@
 using namespace std;
 using namespace cv;
 
+void *myThread(void *arg)
+{
+  int i;
+
+  cv::Mat frame0;
+  cv::Mat frame1;
+  cv::Mat out0 = cv::Mat::zeros( W, H, CV_8UC3);
+  cv::Mat out1 = cv::Mat::zeros( W, H, CV_8UC3);
+  cv::VideoCapture cap0(0);
+  cv::VideoCapture cap1(1);
+
+  cap0.set(CV_CAP_PROP_FRAME_WIDTH, W);
+  cap0.set(CV_CAP_PROP_FRAME_HEIGHT,H);
+  cap1.set(CV_CAP_PROP_FRAME_WIDTH, W);
+  cap1.set(CV_CAP_PROP_FRAME_HEIGHT,H);
+
+  i=0;
+  while(1) {
+	string fileleft="./data/left0";
+	string fileright="./data/right0";
+	string png=".jpg";
+	  
+	cap0 >> frame0;
+	cap1 >> frame1;
+	
+	rotateCW90(frame0, out0, W,H);
+	rotateCW90(frame1, out1, W,H);
+	
+	cv::imshow("Capture0", out0);
+	cv::imshow("Capture1", out1);
+	cvWaitKey(1);
+	
+
+
+	if( cameramutex.a == 1 ) break;
+	else if( cameramutex.a == 2 ) {
+	  cameramutex.lock();
+	  cameramutex.a = 0;
+	  
+	  std::ostringstream file;
+	  file << fileright << i << png;
+	  cv::imwrite(file.str(), out0 );
+	  
+	  std::ostringstream file1;
+	  file1 << fileleft << i++ << png;
+	  cv::imwrite(file1.str(), out1 );
+	  cameramutex.unlock();
+	}
+  }  
+  return NULL;
+}
+
+void *myKey(void *arg)
+{
+  startup();
+  char pwd[PATH_SIZE];
+  char input[500] = {"\0"};
+
+  getcwd(pwd,PATH_SIZE);
+  while(1){
+	cout << NAME_COLOR1 << "arc-->";
+	printf(" $ \x1b[0m");
+	fgets(input,sizeof(input),stdin) ;
+	if( input[0] == 'q' ){ cameramutex.a=1;break;}
+	else if( input[0] == 's' ){ cameramutex.a=2;}
+	else if( input[0] == 'h' ){ help(); }
+	else if( input[0] == 'c' ) { system("clear"); }
+	else if( input[0] == ' '){ cameramutex.a=3;}
+  }
+  end();
+  return NULL;  
+}
+
 int main(int argc, char *argv[])
 {
   int status;
--- src/tracker/src/main.cpp
+++ src/tracker/src/main.cpp
@@ -3,6 +3,109 @@
 using namespace std;
 using namespace cv;
 
+
+void *tracker(void *arg){
+  //video setteing
+  int i;
+  int idx = 0;
+  cv::Mat frame;
+  cv::Mat out = cv::Mat::zeros( W, H, CV_8UC3);
+  cv::VideoCapture cap;
+  string file_save_directory="./data/";
+  string png=".jpg";
+  if (!cap.open(idx)) return 0;
+  else{
+	cap.set(CV_CAP_PROP_FRAME_WIDTH, W);
+	cap.set(CV_CAP_PROP_FRAME_HEIGHT, H);
+  }
+
+  //kalman filter setting
+  double ticks = 0;
+  bool found = false;
+  int notFoundCount = 0;
+  int contrSize = 0;
+  cv::Mat res;
+  cv::KalmanFilter kf(stateSize, measSize, contrSize, CV_32F);
+  cv::Mat state(stateSize, 1, CV_32F);  // [x,y,v_x,v_y,w,h]
+  cv::Mat meas(measSize, 1, CV_32F);    // [z_x,z_y,z_w,z_h]
+  kalman_setting(kf);
+
+  i=0;
+  while (1){
+	//video capture
+	cv::waitKey(1);
+	cap >> frame;
+	cv::Mat blur = cv::Mat::zeros( W, H, CV_8UC3);
+	cv::GaussianBlur(frame, blur, cv::Size(5, 5), 3.0, 3.0);
+	rotateCW90( blur, out, W, H);
+
+	//loop time recorder
+	double precTick = ticks;
+	ticks = (double) cv::getTickCount();
+	double dT = (ticks - precTick) / cv::getTickFrequency();
+	kf.transitionMatrix.at<float>(2) = dT;
+	kf.transitionMatrix.at<float>(9) = dT;
+	//cout << "dT:" << endl << dT << endl;
+
+		
+	//kalman filtering
+	vector<vector<cv::Point> > balls;
+	vector<cv::Rect> ballsBox;
+	cv::Mat rangeRes = cv::Mat::zeros(out.size(), CV_8UC1);
+	out.copyTo( res );
+	if (found) kalman_if_found(kf,state,res);
+	kalman_process(out,rangeRes,res,balls,ballsBox);
+	if (balls.size() == 0)
+	  {
+		if( ++notFoundCount >= 100 ) found = false;
+	  }
+	else
+	  {
+		notFoundCount = 0;
+		kalman_find(meas,ballsBox,found,kf,state);
+	  }
+	cv::imshow("Tracking", res);
+
+
+
+	if( cameramutex.a == 1 ) break;
+	else if( cameramutex.a == 2 ) {
+	  cameramutex.lock();
+	  cameramutex.a = 0;
+	  
+	  std::ostringstream file;
+	  file << file_save_directory << i << png;
+	  cv::imwrite(file.str(), res );
+	  
+	  cameramutex.unlock();
+	}
+  }
+  return 0;
+}
+
+
+
+void *myKey(void *arg)
+{
+  startup();
+  char pwd[PATH_SIZE];
+  char input[500] = {"\0"};
+
+  getcwd(pwd,PATH_SIZE);
+  while(1){
+	cout << NAME_COLOR1 << "arc-->";
+	printf(" $ \x1b[0m");
+	fgets(input,sizeof(input),stdin) ;
+	if( input[0] == 'q' ){ cameramutex.a=1;break;}
+	else if( input[0] == 's' ){ cameramutex.a=2;}
+	else if( input[0] == 'h' ){ help(); }
+	else if( input[0] == 'c' ) { system("clear"); }
+	else if( input[0] == ' '){ cameramutex.a=3;}
+  }
+  end();
+  return NULL;  
+}
+
 int main(int argc, char *argv[])
 {
   int status;
