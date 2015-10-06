42294885b399c413d660cd7b9014641221a52bf4
YusakuSakamoto <yskibouyskibou@gmail.com>
2015-10-06 12:28:07 +0900

    copy original

--- src/meanshift_clustering/src/main.cpp
+++ src/meanshift_clustering/src/main.cpp
@@ -1,36 +1,40 @@
-#include "../../../lib/camera.hpp"
+#include "../include/Meanshift.h"
 
-using namespace std;
 using namespace cv;
+using namespace std;
 
-int main(int argc,char *argv[]){
-  cv::Mat img = cv::imread("./data/input.png",1);
-  //Mean shift
-  int **ilabels = new int *[ img.cols ];
-  for(int i=0;i<img.cols;i++){
-	ilabels[i] = new int [img.rows];
+int main(int argc, char* argv[])
+{
+  IplImage *img = cvLoadImage("./data/input.png");
+
+  // Mean shift
+  int **ilabels = new int *[img->height];
+  for(int i=0;i<img->height;i++){
+	ilabels[i] = new int [img->width];
   }
-  int regionCount = MeanShift(img,ilabels);
+  int regionCount = MeanShift(img, ilabels);
   vector<int> color(regionCount);
-  CvRNG rng = cvRNG(cvGetTickCount());
+  CvRNG rng= cvRNG(cvGetTickCount());
   for(int i=0;i<regionCount;i++){
 	color[i] = cvRandInt(&rng);
   }
   /*
-  for(int i=0;i<img.cols;i++){
-	for(int j=0;j<img.rows;j++){
-	  
-	  int cl = ilabels[i][j];
-	  int a = img.step * i + (j*3);
-	  
-	  img.data[a+0] = (color[cl]) & 255;
-	  img.data[a+1] = (color[cl] >> 8) & 255;
-	  img.data[a+2] = (color[cl] >> 16) & 255;
-	 
-	}
+  // Draw random color
+  for(int i=0;i<img->height;i++){
+	for(int j=0;j<img->width;j++)
+	  { 
+		int cl = ilabels[i][j];
+		((uchar *)(img->imageData + i*img->widthStep))[j*img->nChannels + 0] = (color[cl])&255;
+		((uchar *)(img->imageData + i*img->widthStep))[j*img->nChannels + 1] = (color[cl]>>8)&255;
+		((uchar *)(img->imageData + i*img->widthStep))[j*img->nChannels + 2] = (color[cl]>>16)&255;
+	  }
   }
-  */
-  cv::imshow("img",img);
-  cv::waitKey(0);
+*/
+  cvNamedWindow("MeanShift",CV_WINDOW_AUTOSIZE);
+  cvShowImage("MeanShift",img);
+  cvWaitKey();
+  cvDestroyWindow("MeanShift");
+  cvReleaseImage(&img);
+
   return 0;
 }
